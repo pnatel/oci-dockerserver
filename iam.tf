db9a044b02b46cf84823923f1a4c30ccd958eb4e
Paulo Silva <paulo+webvscode@gsilva.org>
2025-10-03 21:18:46 +1000

    restore policies to cleat access error on decryption

--- iam.tf
+++ iam.tf
@@ -5,12 +5,12 @@ resource "oci_identity_dynamic_group" "oci-id-dynamic-group" {
   matching_rule  = "All {instance.compartment.id = '${oci_identity_compartment.oci-compartment.id}'}"
 }
 
-# resource "oci_identity_policy" "oci-id-instance-policy" {
-#   compartment_id = data.oci_identity_compartment.oci-root-compartment.id
-#   name           = "${var.prefix}-instance-policy"
-#   description    = "Identity Policy for instance to use object storage encryption"
-#   statements     = ["Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to use secret-family in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'", "Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to use vaults in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'", "Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to use keys in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'", "Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to manage object-family in compartment id ${oci_identity_compartment.oci-compartment.id} where target.bucket.name='${var.prefix}-bucket'", "Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to read virtual-network-family in compartment id ${oci_identity_compartment.oci-compartment.id}"]
-# }
+resource "oci_identity_policy" "oci-id-instance-policy" {
+  compartment_id = data.oci_identity_compartment.oci-root-compartment.id
+  name           = "${var.prefix}-instance-policy"
+  description    = "Identity Policy for instance to use object storage encryption"
+  statements     = ["Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to use secret-family in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'", "Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to use vaults in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'", "Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to use keys in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'", "Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to manage object-family in compartment id ${oci_identity_compartment.oci-compartment.id} where target.bucket.name='${var.prefix}-bucket'", "Allow dynamic-group ${oci_identity_dynamic_group.oci-id-dynamic-group.name} to read virtual-network-family in compartment id ${oci_identity_compartment.oci-compartment.id}"]
+}
 
 resource "oci_identity_policy" "oci-id-disk-policy" {
   compartment_id = data.oci_identity_compartment.oci-root-compartment.id
@@ -19,12 +19,12 @@ resource "oci_identity_policy" "oci-id-disk-policy" {
   statements     = ["Allow service blockstorage to use keys in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'"]
 }
 
-# resource "oci_identity_policy" "oci-id-storageobject-policy" {
-#   compartment_id = data.oci_identity_compartment.oci-root-compartment.id
-#   name           = "${var.prefix}-id-storageobject-policy"
-#   description    = "Identity Policy for objectstorage service"
-#   statements     = ["Allow service objectstorage-${local.oci_region} to use keys in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'", "Allow service objectstorage-${local.oci_region} to manage object-family in compartment id ${oci_identity_compartment.oci-compartment.id}"]
-# }
+resource "oci_identity_policy" "oci-id-storageobject-policy" {
+  compartment_id = data.oci_identity_compartment.oci-root-compartment.id
+  name           = "${var.prefix}-id-storageobject-policy"
+  description    = "Identity Policy for objectstorage service"
+  statements     = ["Allow service objectstorage-${local.oci_region} to use keys in compartment id ${oci_identity_compartment.oci-compartment.id} where target.vault.id='${oci_kms_vault.oci-kms-vault.id}'", "Allow service objectstorage-${local.oci_region} to manage object-family in compartment id ${oci_identity_compartment.oci-compartment.id}"]
+}
 
 # resource "oci_identity_group" "oci-bucket-group" {
 #   compartment_id = data.oci_identity_compartment.oci-root-compartment.id
